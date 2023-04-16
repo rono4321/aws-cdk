f5caf707d7c7c96824152963299ce523d4d9655b
Ronodeep Mondal <rono4321@gmail.com>
2023-04-16 20:00:25 +0530

    Added vpc and cognito

--- lib/cdk-test-stack.ts
+++ lib/cdk-test-stack.ts
@@ -5,6 +5,7 @@ import * as s3 from 'aws-cdk-lib/aws-s3'
 import { CfnVPC, Vpc, VpnConnectionBase } from 'aws-cdk-lib/aws-ec2';
 import { VpcSubnetGroupType } from 'aws-cdk-lib/cx-api';
 import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import * as cognito from 'aws-cdk-lib/aws-cognito'
 
 export class CdkTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -18,7 +19,7 @@ export class CdkTestStack extends cdk.Stack {
     });
 
     const s3bucket = new s3.Bucket(this, "ron-test-bucket", {
-      bucketName: "ron-test-bucket-4321",
+      bucketName: "ron-test-bucket-43211",
       accessControl: s3.BucketAccessControl.PRIVATE,
       encryption: s3.BucketEncryption.S3_MANAGED,
       versioned: false,
@@ -38,6 +39,7 @@ export class CdkTestStack extends cdk.Stack {
         value: 'MyVpc',
       }],
     })
+    
 
     const mysubnets =  new ec2.CfnSubnet(this, "subnet1", {
       availabilityZone: "eu-west-1a",
@@ -46,16 +48,92 @@ export class CdkTestStack extends cdk.Stack {
       ipv6Native: false,
       mapPublicIpOnLaunch: false,
       tags: [],
-      vpcId: "vpc-0e9b08d1e797a3d58", // Required
+      vpcId: myvpc.attrVpcId, // Required
     })
 
-    const mysubnets2 = new CfnVPC(this, "subnet2", {
-      cidrBlock: "10.10.2.0/24",
-      enableDnsHostnames: true,
-      enableDnsSupport: true,
-      tags: []
-    })
+    //  const mysubnets2 = new ec2.CfnSubnet(this, "subnet2",{
+    //   vpcId: myvpc.attrVpcId,
+    //   assignIpv6AddressOnCreation: false,
+    //   availabilityZone: "eu-west-1b",
+    //   cidrBlock: "10.10.3.0/24",
+    //   enableDns64: false,
+    //   ipv6Native: false,
+    //   mapPublicIpOnLaunch: false,
 
+
+    //  })
+
+    const userPool = new cognito.UserPool(this, 'userpool', {
+      userPoolName: 'my-user-pool',
+      selfSignUpEnabled: true,
+      signInAliases: {
+        email: true,
+      },
+      autoVerify: {
+        email: true,
+      },
+      standardAttributes: {
+        givenName: {
+          required: true,
+          mutable: true,
+        },
+        familyName: {
+          required: true,
+          mutable: true,
+        },
+      },
+      customAttributes: {
+        country: new cognito.StringAttribute({mutable: true}),
+        city: new cognito.StringAttribute({mutable: true}),
+        isAdmin: new cognito.StringAttribute({mutable: true}),
+      },
+      passwordPolicy: {
+        minLength: 6,
+        requireLowercase: true,
+        requireDigits: true,
+        requireUppercase: false,
+        requireSymbols: false,
+      },
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
+
+    const userPool2 = new cognito.UserPool(this, 'userpool2', {
+      userPoolName: 'my-user-pool2',
+      selfSignUpEnabled: true,
+      signInAliases: {
+        email: true,
+      },
+      autoVerify: {
+        email: true,
+      },
+      standardAttributes: {
+        givenName: {
+          required: true,
+          mutable: true,
+        },
+        familyName: {
+          required: true,
+          mutable: true,
+        },
+      },
+      customAttributes: {
+        country: new cognito.StringAttribute({mutable: true}),
+        city: new cognito.StringAttribute({mutable: true}),
+        isAdmin: new cognito.StringAttribute({mutable: true}),
+      },
+      passwordPolicy: {
+        minLength: 6,
+        requireLowercase: true,
+        requireDigits: true,
+        requireUppercase: false,
+        requireSymbols: false,
+      },
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
+    
+    //  const mysubnets3 = new CfnSubnet
     // const queue1 = new sqs.Queue(this, 'CdkTestQueue1', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });    
@@ -68,6 +146,7 @@ export class CdkTestStack extends cdk.Stack {
 // export class CdkTestStack1 extends cdk.Stack {
 //   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
 //     super(scope, id, props);
+
 
 //     // The code that defines your stack goes here
 
